fa6eb429e1b5d382999964df19cb74d9be55fa30
PlusLius <408709581@qq.com>
2023-08-17 12:33:34 +0800

    fix: package compat

--- packages/react/src/atoms/Color/Color.stories.tsx
+++ packages/react/src/atoms/Color/Color.stories.tsx
@@ -4,6 +4,7 @@ import { text, select } from '@storybook/addon-knobs'
 
 import { Spacing } from '@atomp/foundation'
 
+
 // css
 import '@atomp/scss/lib/Utilities.css'
 
@@ -12,8 +13,9 @@ export default {
 }
 
 export const Common = () => <Color hexCode={text('HexCode', 'pink')} />
-
+const width = select('Width', Object.values(Spacing), 'xxl') as  keyof typeof Spacing
+const height = select('Height', Object.values(Spacing), 'xxl') as  keyof typeof Spacing
 export const CustomDimensions = () => <Color
 hexCode={text('HexCode', 'pink')}
-width={select('Width', Object.values(Spacing), 'xxl')}
-height={select('Height', Object.values(Spacing), 'xxl')}  />
+width={width}
+height={height}  />
